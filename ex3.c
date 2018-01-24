0e3db012faca3a9ea73c06874d4cf2b62f9082a0
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-01-24 16:47:26 +0530

    Create ex3.c

---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int alpha;
+
+scanf("%c",&alpha);
+if(alpha=='a'||alpha=='e'||alpha=='i'||alpha=='o'||alpha=='u')
+printf("Vowel");
+else
+printf("Constants");
+
+}
