192eebab194740bdec8f149e657bb29215c3e45b
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-01-31 16:42:46 +0530

    exno4.c

--- exno4.c
+++ exno4.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main()
 {
-char c;
+char b;
 printf("Enter a character: ");
-scanf("%c",&c);
-if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-printf("%c is an alphabet.",c);
+scanf("%c",&b);
+if( (b>='a'&&b<='z')||(b>='A'&&b<='Z'))
+printf("%c is an alphabet.",b);
 else
-printf("%c is not an alphabet.",c);
+{
+printf("%c is not an alphabet.",b);
+}
 return 0;
 }
