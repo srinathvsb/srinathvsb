d9d098a28a456bb24d1168f687013c903feb53d9
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-04 11:34:56 +0530

    exno14.c

--- exno14.c
+++ exno14.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 void main()
 {
 int num,m =20,n=40;
 clrscr();
 printf("Print Odd Numbers in a given range m to n:\n");
-for (num = m; num <= n; num++)
+for(num=m;num<=n;num++)
 {
-if (num % 2 == 1)
-printf ("%d ", num);
+if(num%2==1)
+printf("%d ",num);
 }
 getch();
 }
