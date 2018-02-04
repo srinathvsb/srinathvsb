6374861121d61a861c8b0468ba7be0950c9a8bdb
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-04 11:24:30 +0530

    exno13.c

---
+++ exno13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int n, i,prime= 0;
+printf("Enter a positive integer: ");
+scanf("%d",&n);
+for(i=2; i<=n/2; ++i)
+{
+if(n%i==0)
+{
+prime=1;
+break;
+}
+}
+if(prime==0)
+printf("%d is a prime number.",n);
+else
+printf("%d is not a prime number.",n);
+return 0;
+}
