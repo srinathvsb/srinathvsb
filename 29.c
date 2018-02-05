78feca423536a3e286b7922c128c226bf1643055
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-05 16:39:05 +0530

    29.c

--- 29.c
+++ 29.c
@@ -3,10 +3,10 @@ int main()
 {
 int time=0;
 int min=0;
-int hours=0;
+int hour=0;
 time=640;
 minutes=time%60;
 hours=(time-minutes)/60;
-printf("total minutes %d=%d hours and %d minutes\n",time,hours,minutes);
+printf("total minutes %d=%d hours and %d minutes\n",time,hour,minutes);
 return(0);
 }
