bd94b7b5dd3b5ec31b2d8a0d4859f7c536fa56f3
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-05 16:18:26 +0530

    23.c

--- 23.c
+++ 23.c
@@ -4,20 +4,14 @@ int main()
 int i,j,a[10],temp,n;
 printf("enter the array size\n");
 scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
-for(i=0;i<n-1;i++)
-{
-for(j=1;j<n;j++)
-{
-if(a[i]<a[j])
-{
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);}
+for(i=0;i<n-1;i++){
+for(j=1;j<n;j++){
+if(a[i]<a[j]){
 temp=a[i];
 a[i]=a[j];
-a[j]=temp;
-}
+a[j]=temp;}
 }
 }
 printf("%d",&a[0]);
