682364fe50400000be85f5fcc3c8bbec9eb2cda2
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-05 15:46:27 +0530

    armstrongnumber.c

--- exno18.c
+++ exno18.c
@@ -4,9 +4,9 @@ int main()
 {
 int low,high,i,temp1,temp2,remainder,n=0,result=0;
 printf("Enter two numbers(intervals): ");
-scanf("%d %d", &low, &high);
+scanf("%d%d",&low,&high);
 printf("Armstrong numbers between %d an %d are: ", low, high);
-for(i = low + 1; i < high; ++i)
+for(i=low+1;i<high;++i)
 {
 temp2=i;
 temp1=i;
@@ -19,18 +19,14 @@ while(temp2 != 0)
 {
 remainder=temp2 % 10;
 result+=pow(remainder,n);
-            temp2/=10;
-        }
-
-        // checks if number i is equal to the sum of nth power of its digits
-        if (result == i) {
-            printf("%d ", i);
-        }
-
-        // resetting the values to check Armstrong number for next iteration
-        n = 0;
-        result = 0;
-
-    }
-    return 0;
+temp2/=10;
+}
+if(result==i) 
+{
+printf("%d",i);
+}
+n=0;
+result=0;
+}
+return 0;
 }
