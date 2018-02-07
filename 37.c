1f9573037cc1a29bce2f69211ec0e0b466b6dc44
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-07 16:07:55 +0530

    37.c

---
+++ 37.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int j,i,t;
+printf("\n initialise the values");
+printf("\n enter the first number");
+scanf("%d",&j);
+printf("\n enter the second number");
+scanf("%d",&i);
+t=j;
+j=i;
+k=t;
+printf("\n after swapping %d %d",j,i);
+return 0;
+}
