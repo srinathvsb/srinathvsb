a0237c3e29150ad18040d30b4db61337e1fedce2
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-09 16:44:54 +0530

    48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int a,i;
+printf("enter the a value");
+scanf("%d",&a);
+for(i=1;i<a;i++)
+{
+printf("%d",i);
+}
+if=(a/2!=0)
+{
+printf("the average value is");
+}
+getch();
+}
