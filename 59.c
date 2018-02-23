46226c655d627d71db3afd1987eefda61feeb8eb
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-23 15:57:31 +0530

    59.c

---
+++ 59.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,max=0,i,arr[10];
+max=a[0];
+for(i=0;i<10;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+if(arr[i]>max)
+max=arr[i];
+}
+printf("%d",max);
+return 0;
+}
