86f673df8be758839ffc01dea4df468e38f02c51
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-26 15:39:59 +0530

    66.c

---
+++ 66.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+int n,i,count=0;
+scanf("%d",&n);
+for(i=2;i<=(n/2);i++)
+{
+if(n%i==0)
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("yes...prime");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
