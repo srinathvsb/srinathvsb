6db1586ce7dd15e82ed8727ea7d666cc05f5020e
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-02-28 15:44:48 +0530

    73.c

---
+++ 73.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() {
+int a,b,c;
+printf("Enterb the number\n");
+scanf("%d",&a);
+printf("Enter the range");
+scanf("%d %d",&b,&c);
+if(a>b&&a<c)
+printf("Yes");
+else
+printf("No");
+}
