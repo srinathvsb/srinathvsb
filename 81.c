369daa0880e9d8e54eb14a89e3e76d1e1579cfd2
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-03-02 16:32:49 +0530

    81.c

---
+++ 81.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int ninja,opponent,difference;
+scanf("%d",&ninja);
+scanf("\n%d",&opponent);
+difference=ninja-opponent;
+ printf("%d",difference);
+ }
