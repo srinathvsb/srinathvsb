f948abc6ab12dd24de6fb8248427d6faa3bc017d
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-03-02 16:13:12 +0530

    79.c

--- 79.c
+++ 79.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-int a,b;
-scanf("%d%d",&a,&b);
-printf("The product is:%d",a*b);
-if(a/b==1)
+int x,y;
+scanf("%d%d",&x,&y);
+printf("The product is:%d",x*y);
+if(x/y==1)
 printf("Perfect Square");
 else
 printf("Not perfect square");
