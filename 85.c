266a62bba1b6b4d5bef18c384945b703ae72a2c7
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-03-05 15:59:14 +0530

    85.c

--- 85.c
+++ 85.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<string.h>
 int main(void)
-
 {
 char ch[100],a[100],b[100];
 gets(ch);
@@ -9,18 +8,16 @@ int i,k;
 k=strlen(ch);
 for(i=0;i<k;i++)
 {
-	if(i%2==0)
-	printf("%c",ch[i]);
-	
+if(i%2==0)
+printf("%c",ch[i]);
 }
 printf(" ");
 for(i=0;i<k;i++)
 {
-	if(i%2!=0)
-	{
-		
-		printf("%c",ch[i]);
-	}
+if(i%2!=0)
+{
+printf("%c",ch[i]);
 }
-	return 0;
+}
+return 0;
 }
