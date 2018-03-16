a80338b6ccd43647cfdfb20d4ab1a55f4d364bc4
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-03-16 16:20:48 +0530

    113.c

--- 113.c
+++ 113.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(void) 
 {
-int i,n,k,array[50],count=0;
-scanf("%d %d",&n,&k);
+int i,n,j,array[50],count=0;
+scanf("%d %d",&n,&j);
 for(i=0;i<n;i++)
 {
 scanf("%d",&array[i]);
 }
 for(i=0;i<n;i++)
 {
-if(array[i]==k)
+if(array[i]==j)
 {
 count=count+1;
 }
