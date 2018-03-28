842df0d59514315d0c4712a15a1b7185240fb2c1
srinathvsb <30748978+srinathvsb@users.noreply.github.com>
2018-03-28 16:08:13 +0530

    p21.c

---
+++ p21.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int x1=2,y1=3,x2=4,y2=5,x3=6,y3=7;
+x1=int(input("x1 value:"))
+x2=int(input("x2 value:"))
+y1=int(input("y1 value:"))
+y2=int(input("y2 value:"))
+x3=int(input("y3 value:"))
+if(x1=x2=x3) || (y1=y2=y3)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
